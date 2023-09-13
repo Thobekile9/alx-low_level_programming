7dafe1a0623e12dbae24aa72e6930872f86b40c0
Thobekile <thobekilemagaya995@gmail.com>
2023-09-13 04:32:53 -0700

    4-isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -3,7 +3,7 @@
 /**
  *_isalpha - checks for alphabetic character
  * @c: The character to be checked
- Return: Always 1 for alphabetic character or 0 for anything else
+ * Return: Always 1 for alphabetic character or 0 for anything else
  */
 int _isalpha(int c)
 {
